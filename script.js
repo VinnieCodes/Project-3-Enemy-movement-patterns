b796cc87e51d924e1c04c9c7e04ce53cd36e0e18
Vinnie <vincentathorne2005@gmail.com>
2025-01-19 12:43:30 -0800

    bats done

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-CANVAS_WIDTH = canvas.width = 500;
-CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+CANVAS_WIDTH = canvas.width = 1000;
+CANVAS_HEIGHT = canvas.height = 500;
+const numberOfEnemies = 30;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = "enemy1.png";
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+    this.image = new Image();
+    this.image.src = "enemy1.png";
     this.spriteWidth = 293;
     this.spriteHeight = 155;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
-    this.speed = Math.random() * 4 - 2;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    // this.speed = Math.random() * 4 - 2;
     this.frame = 1;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    let jiggleSpeed = 2
+    this.x += Math.random() * (2 * jiggleSpeed) - jiggleSpeed;
+    this.y += Math.random() * (2 * jiggleSpeed) - jiggleSpeed;
     // animate sprites
-    if (gameFrame % 2 === 0) {
-      this.frame > 4 ? this.frame = 0 : this.frame++;
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
-    
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
-      enemyImage,
+      this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
